4dcc619c4849fb99f72cbb44b53b1461a5ec7a81
azmaveth <hysun@hysun.com>
2026-09-24 17:14:34 -0500

    test(actions): require valid scheduler listing descriptor

---
+++ apps/arbor_actions/test/arbor/actions/scheduler_descriptor_regression_test.exs
@@ -0,0 +1,11 @@
+defmodule Arbor.Actions.SchedulerDescriptorRegressionTest do
+  use ExUnit.Case, async: true
+
+  @moduletag :fast
+
+  test "security regression: routine listing has an admissible read descriptor" do
+    assert {:ok, descriptor} = Arbor.Actions.runtime_descriptor(Arbor.Actions.Scheduler.ListRoutines)
+    assert descriptor["effect_class"] == "read"
+    assert descriptor["name"] == "scheduler_list_routines"
+  end
+end
