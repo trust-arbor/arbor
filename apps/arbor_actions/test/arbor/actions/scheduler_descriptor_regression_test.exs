4b8b15c47faf27f9ed1eb45922f7be9cf3a8a05f
azmaveth <hysun@hysun.com>
2026-09-24 20:00:57 -0500

    test: format scheduler descriptor assertion

--- apps/arbor_actions/test/arbor/actions/scheduler_descriptor_regression_test.exs
+++ apps/arbor_actions/test/arbor/actions/scheduler_descriptor_regression_test.exs
@@ -4,7 +4,9 @@ defmodule Arbor.Actions.SchedulerDescriptorRegressionTest do
   @moduletag :fast
 
   test "security regression: routine listing has an admissible read descriptor" do
-    assert {:ok, descriptor} = Arbor.Actions.runtime_descriptor(Arbor.Actions.Scheduler.ListRoutines)
+    assert {:ok, descriptor} =
+             Arbor.Actions.runtime_descriptor(Arbor.Actions.Scheduler.ListRoutines)
+
     assert descriptor["effect_class"] == "read"
     assert descriptor["name"] == "scheduler_list_routines"
   end
